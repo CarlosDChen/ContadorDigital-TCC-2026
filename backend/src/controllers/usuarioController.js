740aa783988f46b48c3ef6dff6aeb1252c88a04a
gustavo <guvergara18@gmail.com>
2026-05-12 19:24:24 -0300

    Cria API GET de usuarios

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -1,17 +1,31 @@
+const Usuario = require('../models/usuarioModel')
+
 const listarUsuarios = (req, res) => {
-    const usuarios = [
-        {
-            idUsuario: 1,    
-            nomeUso: "Joaquim"
-        },
-        {
-            idUsuario: 2,
-            nomeUso: "Maria"
-        }
-    ]
-    res.json(usuarios)
+  const usuario1 = new Usuario(
+    1,
+    '12345678900',
+    'joao@email.com',
+    '11999999999',
+    'Joao',
+    'jojo',
+    '123456'
+  )
+
+  const usuario2 = new Usuario(
+    2,
+    '98765432100',
+    'maria@email.com',
+    '11888888888',
+    'Maria',
+    'Ma',
+    '654321'
+  )
+
+  const usuarios = [usuario1, usuario2]
+
+  res.json(usuarios)
 }
 
 module.exports = {
-    listarUsuarios
+  listarUsuarios
 }
